cb89fe0c5fc2cf8081a64b47e06d460fc1c4bec2
3dw1nM0535 <workockmoses@gmail.com>
2017-11-13 11:44:59 +0300

    port number redefined

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ var app = express();
 //Routes Middleware
 app.use('/api', require('./Routes/api'));
 
-
-
-
+//Port number def
 var port = 1234 || process.env.PORT;
+
 //listen for request on port 1234
 app.listen(port, function () {
   console.log('Listening for requests!');
