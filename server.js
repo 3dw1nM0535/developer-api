db36e8379df3c446c21944d5679ab69fb8c19867
3dw1nM0535 <workockmoses@gmail.com>
2017-11-13 11:43:38 +0300

    routes Middleware setup

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require('express');
 
 //instantiate express app
 var app = express();
+
+//Routes Middleware
+app.use('/api', require('./Routes/api'));
+
+
+
+
 var port = 1234 || process.env.PORT;
-
-
 //listen for request on port 1234
 app.listen(port, function () {
   console.log('Listening for requests!');
