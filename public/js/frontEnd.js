8c3fa42ff9dfbd360968cc6d1c70e0a2ed94f826
3dw1nM0535 <workockmoses@gmail.com>
2017-11-14 14:27:36 +0300

    created react component

---
+++ public/js/frontEnd.js
@@ -0,0 +1,53 @@
+var Dev = React.createClass({
+
+  getInitialState: function () {
+    return ({
+      devs: []
+    });
+  },
+
+  render: function () {
+    var devs = this.state.devs;
+    devs = devs.map(function (dev, i) {
+      return (
+        <li key={i}>
+          <span className={dev.obj.available}></span>
+          <span className="name">{dev.obj.name}</span>
+          <span className="rank">{dev.obj.rank}</span>
+          <span className="dist">{Math.floor(dev.dis/ 1000)} Km</span>
+        </li>
+      );
+    })
+    return (
+      <div id="ninja-container">
+        <form id="search" onSubmit={this.handleSubmit}>
+          <label>Enter your latitude:</label>
+          <input type="text" ref="lat" placeholder="latitude" required />
+          <label>Enter your longitude:</label>
+          <input type="text" ref="lng" placeholder="longitude" required />
+          <input type="submit" value="Find Developer" />
+        </form>
+        <ul>
+          {devs}
+        </ul>
+      </div>
+    );
+  },
+
+  handleSubmit: function (e) {
+    e.preventDefault();
+    var lng = this.refs.lng.value;
+    var lat = this.refs.lat.value;
+
+    fetch('/api/dev?lng=' + lng + '&lat=' + lat).then(function (data) {
+      return data.json();
+    }).then(json => {
+      this.setState({
+        devs: json
+      });
+    });
+  }
+
+});
+
+ReactDOM.render(<Dev />, document.getElementById('root'));
